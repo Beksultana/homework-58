1c7b16130b4a8fc1f471f1a7af61913e3180502b
sultan <aidaraliuulu@gmail.com>
2019-01-14 13:04:40 +0600

    Создал компонент Modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Modal from './components/Modal/Modal.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="Container">
+          <Modal>
+            <h1>Some kinda modal title</h1>
+              <p><strong>This is modal content</strong></p>
+          </Modal>
       </div>
     );
   }
